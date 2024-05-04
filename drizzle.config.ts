024a079cf0bdf69c2c82f49ab3ccfdcfea2dea0d
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-05-05 04:27:19 +0530

    Init

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./server/db/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: env.DATABASE_URL,
+  },
+  tablesFilter: ["transactions_*"],
+} satisfies Config;
