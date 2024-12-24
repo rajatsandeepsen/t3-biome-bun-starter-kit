1989a3a3e954960e5151e7bee4b9e8ef1bf4c2f6
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-12-24 22:04:03 +0530

    feat: added tailwindcss-motion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss"
 
 const config = {
-  darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
@@ -74,7 +73,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("tailwindcss-motion")],
 } satisfies Config
 
 export default config
