e7233d3a1711a1bb63efb39054c551d43b986aeb
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2025-01-28 16:31:14 +0530

    feat: million.js compiler

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
+import { next } from 'million/compiler';
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./env.js");
+// await import("./env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-	experimental: {
-        typedRoutes: true,
-		reactCompiler: true,
-	},
+  experimental: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
 };
 
-export default config;
+// @ts-ignore
+export default next(config, {
+  auto: true,
+  mute: true,
+});
--- trpc/server.ts
+++ trpc/server.ts
@@ -1,18 +1,18 @@
-import "server-only";
+import 'server-only';
 
-import { headers } from "next/headers";
-import { cache } from "react";
+import { headers } from 'next/headers';
+import { cache } from 'react';
 
-import { createCaller } from "@/server/api/root";
-import { createTRPCContext } from "@/server/api/trpc";
+import { createCaller } from '@/server/api/root';
+import { createTRPCContext } from '@/server/api/trpc';
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(() => {
-  const heads = new Headers(headers());
-  heads.set("x-trpc-source", "rsc");
+const createContext = cache(async () => {
+  const heads = new Headers(await headers());
+  heads.set('x-trpc-source', 'rsc');
 
   return createTRPCContext({
     headers: heads,
